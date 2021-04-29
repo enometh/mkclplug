b383ca3d000d7b252028b15d0431edc187a0db31
Madhu <enometh@net.meer>
2021-04-29 14:54:34 +0530

    mkclplugtest_initrc: commented out e.g. monitor and load another file

--- mkclplugtest_initrc.lisp
+++ mkclplugtest_initrc.lisp
@@ -1,3 +1,36 @@
 (in-package "COMMON-LISP-USER")
 (format t "Now Playing ~S ...~&" *load-pathname*)
 
+#+nil
+(progn
+  (require 'cmp)
+  ;; set up the desired environment
+  (load "~/.mkcl")
+  (require 'cffi)
+
+  (require 'slynk)
+  ;; start slynk at a port (connect to it from emacs)
+  (unless (find :slynk *features*)
+    (slynk-start)))
+
+#||
+;; e.g. load and Monitor another file.  (Note you can't call MKCLPLUG's
+;; loadlispfile from the slynk thread)
+
+(cffi:defcallback load-lisp-file :void ((path :string))
+  (format t "load-lisp-file ~A~&" path)
+  (prog nil
+     (handler-bind ((error (lambda (error)
+			      (write-lisp-backtrace error)
+			      (return))))
+       (load path))))
+
+(cffi:defcfun (load-and-monitor "load_and_monitor") :void
+    (lispfilepath :pointer)
+    (watchedcb :pointer)
+    (unwatchp :boolean))
+
+(cffi:with-foreign-string (initrc "/dev/shm/local-initrc.lisp")
+  (load-and-monitor initrc (cffi:callback load-lisp-file)
+		    nil))
+||#
