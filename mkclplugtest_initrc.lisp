6f8bd281a76ef3cdff3096d7834acefb84d7bec8
Madhu <enometh@net.meer>
2021-05-02 06:36:44 +0530

    mkclplugtest_initrc: rework sample code
    Instead of starting up slynk from the initrc, monitor a file
    in a runtime directory e.g. /dev/shm/<uid>-<app>-runtime/<pid>.lisp
    
    and write code that starts up a slynk listener on a semi-random port
    to this file

--- mkclplugtest_initrc.lisp
+++ mkclplugtest_initrc.lisp
@@ -1,21 +1,34 @@
 (in-package "COMMON-LISP-USER")
 (format t "Now Playing ~S ...~&" *load-pathname*)
 
-#+nil
 (progn
   (require 'cmp)
   ;; set up the desired environment
   (load "~/.mkcl")
-  (require 'cffi)
+  (require 'cffi))
 
+#+nil
+(progn
   (require 'slynk)
   ;; start slynk at a port (connect to it from emacs)
   (unless (find :slynk *features*)
     (slynk-start)))
 
+
 #||
-;; e.g. load and Monitor another file.  (Note you can't call MKCLPLUG's
-;; loadlispfile from the slynk thread)
+;; load and Monitor another file.  (Note you can't call MKCLPLUG's
+;; loadlispfile from the slynk thread). also
+;; (argv 0) doesn't work for some reason
+||#
+
+(defun runtime-initrc-path (&key (pid (mkcl:getpid))
+			    (uid (mkcl:getuid))
+			     (app-name
+			      (cffi:foreign-string-to-lisp
+			       (cffi:mem-ref
+				(cffi:foreign-symbol-pointer "stashed_appname")
+				:pointer))))
+  (format nil "/dev/shm/~D-~A-runtime/~D.lisp" uid app-name pid))
 
 (cffi:defcallback load-lisp-file :void ((path :string))
   (format t "load-lisp-file ~A~&" path)
@@ -30,7 +43,30 @@
     (watchedcb :pointer)
     (unwatchp :boolean))
 
-(cffi:with-foreign-string (initrc "/dev/shm/local-initrc.lisp")
+
+(cffi:with-foreign-string (initrc (runtime-initrc-path))
   (load-and-monitor initrc (cffi:callback load-lisp-file)
 		    nil))
-||#
+
+(progn
+  (ensure-directories-exist (runtime-initrc-path))
+  (unless (probe-file  (runtime-initrc-path))
+(with-open-file (stream (runtime-initrc-path) :direction :output
+			:if-exists :supersede)
+  (write `(in-package "CL-USER") :stream stream)
+  (terpri stream)
+  (write `(format t "NOW PLAYING ~A~&" *load-pathname*) :stream stream)
+  (terpri stream)
+  (write `(progn
+	    (load "~/sly-config")
+	    (require 'slynk)
+	    (defvar +pid-start+ 4005)
+	    (defvar *slynk-pid* (mkcl::getpid))
+	    (defvar *slynk-port* (+ *slynk-pid* +pid-start+))
+	    (slynk::create-server :port *slynk-port*)
+	    (setf (slynk::debug-on-slynk-error) t)
+	    (defun logpid (fmt &rest args)
+	      (format t "[~D] ~?~&" *slynk-pid* fmt args))
+	    (logpid "Slynk listening on port ~A (~A + ~A)" *slynk-port* *slynk-pid* +pid-start+))
+	 :stream stream)
+  (terpri stream))))
