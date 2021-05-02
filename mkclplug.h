c7108d02c5454d37df59912a90e8263062ab14ac
Madhu <enometh@net.meer>
2021-05-02 06:36:00 +0530

    mkclplug.c - add  mkcl_init_module

--- mkclplug.h
+++ mkclplug.h
@@ -2,3 +2,4 @@ extern void mkcl_initialize (char *app);
 extern int mkcl_shutdown ();
 //extern void mkcl_repl ();
 extern void mkcl_eval(mkcl_env, char *fmt, ...);
+extern mkcl_object mkcl_init_module(void (*entry_point)(MKCL, mkcl_object, mkcl_object));
