c86b78e3d3a84b61f335b09d5cd476442b5f8349
Madhu <enometh@net.meer>
2021-05-09 22:22:40 +0530

    mkclplug refactor mkcl_initialize

--- examples/wkmkclext/wkmkclext-check-init.c
+++ examples/wkmkclext/wkmkclext-check-init.c
@@ -2,17 +2,17 @@
 #include <mkcl/mkcl.h>
 #include "mkclplug.h"
 
-extern void init_wkmkclext(MKCL, mkcl_object, mkcl_object);
+extern void init_wkmkclext (MKCL, mkcl_object, mkcl_object);
 
 const char *
-g_module_check_init(GModule *module)
+g_module_check_init (GModule * module)
 {
-  g_message("g_module_check_init(%s):", g_module_name(module));
-  mkcl_initialize("wkmkclext");
+  g_message ("g_module_check_init(%s):", g_module_name (module));
   void *loc;
-  if (!g_module_symbol(module, "init_wkmkclext", &loc)) {
-    return "failed to provide init_wkmkclext";
-  }
-  mkcl_init_module(init_wkmkclext);
+  if (!g_module_symbol (module, "init_wkmkclext", &loc))
+    {
+      return "failed to provide init_wkmkclext";
+    }
+  mkcl_initialize_module ("wkmkclext", init_wkmkclext);
   return NULL;
 }
