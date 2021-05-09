63ee3180f84cf6595cbdc408c54fbc57d93dbdde
Madhu <enometh@net.meer>
2021-05-09 22:22:40 +0530

    mkclplug refactor mkcl_initialize

--- mkclplug.h
+++ mkclplug.h
@@ -1,5 +1,6 @@
 extern void mkcl_initialize (char *app);
 extern int mkcl_shutdown ();
-//extern void mkcl_repl ();
-extern void mkcl_eval(mkcl_env, char *fmt, ...);
-extern mkcl_object mkcl_init_module(void (*entry_point)(MKCL, mkcl_object, mkcl_object));
+extern void mkcl_eval (mkcl_env, char *fmt, ...);
+extern void mkcl_initialize_module (char *app,
+				    void (*entry_point) (MKCL, mkcl_object,
+							 mkcl_object));
