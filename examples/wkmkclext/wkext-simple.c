f68709a305e2030f770dc3a1769a311c6caf8dd5
Madhu <enometh@net.meer>
2022-01-04 00:23:17 +0530

    examples/wkmkclext: wkmkclext-simple
    I wasn't able to debug the failures with the mkcl-dumped
    libwkmkclext-library.so.  So the older approach is implemented as an
    alternative.
    
    A webkit webextension module called wkmkclext-simple.so is produced
    from C.  wkmkclext.lisp still defines the webkit initialization
    function as a cffi callback but this is not exported through ELF.
    Instead we arrange for this function to be called by the C-defined
    `webkit_web_extension_initialize'.  This is done by passing the
    callback address to C when wkmkclext.lisp is loaded when mkcl is
    initialized through the mkclplug module loading facility.
    
    * examples/wkmkclext/Makefile: (wkmkclext-simple.so): new target.  a
    webkit-web-extension loadable module built with C.
    
    * examples/wkmkclext/wkext-simple.c:
    (register_init1): takes a single argument to store the address of a
    lisp callback function which initializes the webkit side and stashes
    it. (webkit_web_extension_initialize): call the stashed function.
    
    wkmkclext-simple.o doesn't need to know about any lisp since there
    isn't any initialization. Generate it from wkext-simple.o by defining
    WKEXT_APPNAME='"wkmkclext"'
    
    There is need for a check-init-simple.c since there is no need for CL
    initialization. Only the webkit extension has to be initialized.
    GModuleCheck facility does just that. handle stub init via -DWK_MKCL
    
    * examples/wkmkclext/wkmkclext.lisp: conditionalize on :wkmkclext-simple
    in *features*.
    
    ;madhu 221211 fixed by arranging to set the environment variable
    WKEXTSIMPLE (in wkext-simple.c:(init_wkmkclext)), and using this to
    set the lisp feature when loading initrc
    
    ;madhu 221229
    
    * examples/wkmkclext/wkmkclext.system: we try to detect problems with
    compile-time mismatch of the :wkmkclext-simple feature, but don't take
    any chances: if :wkmkclext-simple is in features don't compile
    wkmkclext.lisp always load it.

---
+++ examples/wkmkclext/wkext-simple.c
@@ -0,0 +1,40 @@
+// alternative to not using export-cname.
+
+#include <stdio.h>
+#include <webkit2/webkit-web-extension.h>
+
+#ifdef WK_MKCL
+#include <mkclplug.h>
+void init_wkmkclext(MKCL, mkcl_object cblock, mkcl_object fasl_filename)
+{
+  g_setenv("WKEXTSIMPLE", "1", TRUE);
+}
+#endif
+
+void (*init1)(void *webkitwebextension) = NULL;
+/*
+ * (cffi:defcallback initialize-webextension :void ((webextension :pointer)))
+ * (cffi:foreign-funcall "register_init1" :pointer
+ *    (cffi:callback initialize-webextension) :void)
+ */
+void
+register_init1(void (*func)(void *))
+{
+	init1 = func;
+}
+
+G_MODULE_EXPORT void
+webkit_web_extension_initialize (WebKitWebExtension *extension)
+{
+	fprintf(stderr,
+#ifdef WK_MKCL
+		"wkmkclext"
+#endif
+		" initialize\n");
+
+	if (init1) {
+		init1(extension);
+	} else {
+	  g_warning("lisp side not initalized");
+	}
+}
