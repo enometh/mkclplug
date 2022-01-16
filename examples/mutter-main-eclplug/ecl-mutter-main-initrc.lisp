3c3560913bbcb838254b4af8ca9cb8e214ab4271
Madhu <enometh@net.meer>
2022-01-16 13:22:25 +0530

    mutter-main-eclplug

---
+++ examples/mutter-main-eclplug/ecl-mutter-main-initrc.lisp
@@ -0,0 +1,21 @@
+;;; -*- Mode: LISP; Package: :cl-user; BASE: 10; Syntax: ANSI-Common-Lisp; -*-
+;;;
+;;;   Time-stamp: <>
+;;;   Touched: Sun Jan 16 11:51:45 2022 +0530 <enometh@net.meer>
+;;;   Bugs-To: enometh@net.meer
+;;;   Status: Experimental.  Do not redistribute
+;;;   Copyright (C) 2022 Madhu.  All Rights Reserved.
+;;;
+(in-package "CL-USER")
+(format t ";;; Now playing ~S~&" *load-pathname*)
+(pushnew :wk *features*)
+
+#||
+(defvar *local-init-loaded* nil)
+(unless *local-init-loaded*
+  (load "~/cl/mkclplug/examples/mutter-main-eclplug/local-init.lisp")
+  (setq *local-init-loaded* t))
+
+(load "~/cl/mkclplug/examples/mutter-main-eclplug/mutter-main-eclplug.system")
+(require 'mutter-main-eclplug)
+||#
