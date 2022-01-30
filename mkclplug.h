298c70b95d1b98884c673e80941a232bbaf97114
Madhu <enometh@net.meer>
2022-01-30 09:59:57 +0530

    mkcl_eval: change signature to drop the mkcl_env argument.
    yes this is the wrong thing to do. However the goal is to isolate the
    user of mkclplug from threading issues.

--- mkclplug.h
+++ mkclplug.h
@@ -1,6 +1,7 @@
+#include <mkcl/mkcl.h>
 extern void mkcl_initialize (char *app);
 extern int mkcl_shutdown ();
-extern void mkcl_eval (mkcl_env, char *fmt, ...);
+extern void mkcl_eval (char *fmt, ...);
 extern void mkcl_initialize_module (char *app,
 				    void (*entry_point) (MKCL, mkcl_object,
 							 mkcl_object));
