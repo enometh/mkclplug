05226fc7b049e50e3a0f29baac2e76341371b17f
Madhu <enometh@net.meer>
2022-02-05 20:22:14 +0530

    eclplug: ecl_initialize_module parity with mkclplug

--- eclplug.h
+++ eclplug.h
@@ -1,3 +1,5 @@
+#include <ecl/ecl.h>
 extern void ecl_initialize (char *app);
 extern int ecl_shutdown ();	/* for use in atexit */
 extern void ecl_eval (char *fmt, ...);
+extern void ecl_initialize_module (char *app, void (*entry) (cl_object));
