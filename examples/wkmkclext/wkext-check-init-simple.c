3941ef8bde4a98ee85ccf51460d1691d4fb90991
Madhu <enometh@net.meer>
2022-02-11 11:47:04 +0530

    squash! examples/wkmkclext: wkmkclext-simple
    get rid of wkext-check-init-simple.c
    stub init via -DWK_MKCL

--- examples/wkmkclext/wkext-check-init-simple.c
+++
@@ -1,7 +0,0 @@
-#include <gmodule.h>
-const char *
-g_module_check_init (GModule * module)
-{
-  g_message ("g_module_check_init(%s):", g_module_name (module));
-  return NULL;
-}
--- examples/wkmkclext/wkext-simple.c
+++ examples/wkmkclext/wkext-simple.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <webkit2/webkit-web-extension.h>
 
+#ifdef WK_MKCL
+#include <mkclplug.h>
+void init_wkmkclext(MKCL, mkcl_object cblock, mkcl_object fasl_filename)
+{
+}
+#endif
+
 void (*init1)(void *webkitwebextension) = NULL;
 /*
  * (cffi:defcallback initialize-webextension :void ((webextension :pointer)))
@@ -18,7 +25,12 @@ register_init1(void (*func)(void *))
 G_MODULE_EXPORT void
 webkit_web_extension_initialize (WebKitWebExtension *extension)
 {
-	fprintf(stderr, WKEXT_APPNAME " initialize\n");
+	fprintf(stderr,
+#ifdef WK_MKCL
+		"wkmkclext"
+#endif
+		" initialize\n");
+
 	if (init1) {
 		init1(extension);
 	} else {
